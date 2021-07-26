d7ca01be43838aad60b4c2c47e2bca28c96e0838
Colton Johnson <cj236@njit.edu>
2021-07-26 01:29:32 -0400

    Initial Commit

---
+++ WeatherFinal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherFinal
+//
+//  Created by Colton Johnson on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherFinal/WeatherFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherFinalApp.swift
+//  WeatherFinal
+//
+//  Created by Colton Johnson on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
